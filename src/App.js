54d4fb8ea2ae1a24cb06e5658615f33f9e7a22f9
django1858 <saravananv123@outlook.com>
2021-08-12 01:28:28 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Yo yo yo 148 to the 3 to the 3 to the 6 to the 9! 
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </p>
+        <p>watup biatch?</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
